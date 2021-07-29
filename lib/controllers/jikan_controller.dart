1a384627549a5d4eefbde81296e0a079f0f955b4
Javin <javin.tan.20@usjr.edu.ph>
2021-07-29 10:48:57 +0800

    Search Query Created

--- lib/controllers/jikan_controller.dart
+++ lib/controllers/jikan_controller.dart
@@ -1,3 +1,4 @@
+import 'package:anithing/constants/genres.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import '../services/jikan_service.dart';
@@ -13,10 +14,38 @@ class JikanController extends GetxController {
   RxList<Manga> mangaList = <Manga>[].obs;
   Anime retrievedAnime = Anime();
 
-  TextEditingController searchQueryController = TextEditingController();
   RxString mode = "Anime".obs;
+  TextEditingController searchQueryTextController = TextEditingController();
+  RxList<bool> selectedAnimeGenres = List<bool>.filled(AnimeGenres.values.length, false).obs;
+  RxList<bool> selectedMangaGenres = List<bool>.filled(MangaGenres.values.length, false).obs;
 
   void getAnime(int animeId) {
     Anime retrievedAnime = fetchAnime(animeId);
+  }
+
+  void searchQuery({int page = 1}) {
+    // TODO: Require search entry to be either empty or at least 3 Letters!
+    print("=====Creation of Search Query=====");
+    String searchQuery = "${mode.value == "Anime" ? "anime" : "manga"}?q=${searchQueryTextController.text}&page=$page";
+    String genresQuery = "";
+    if (mode.value == "Anime") {
+      if (selectedAnimeGenres.contains(true)) {
+        genresQuery = "&genre=";
+        for (var i = 0; i < AnimeGenres.values.length; i++) {
+          if (selectedAnimeGenres[i]) genresQuery += (AnimeGenres.values[i + 1].index.toString() + ',');
+        }
+        genresQuery = genresQuery.replaceFirst(",", "", genresQuery.length - 1);
+      }
+    } else {
+      if (selectedMangaGenres.contains(true)) {
+        genresQuery = "&genre=";
+        for (var i = 0; i < MangaGenres.values.length; i++) {
+          if (selectedMangaGenres[i]) genresQuery += (MangaGenres.values[i + 1].index.toString() + ',');
+        }
+        genresQuery = genresQuery.replaceFirst(",", "", genresQuery.length - 1);
+      }
+    }
+    searchQuery += genresQuery;
+    print(searchQuery);
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -25,17 +25,19 @@ class Home extends StatelessWidget {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1, vertical: 10),
               child: TextField(
-                controller: jikanController.searchQueryController,
+                controller: jikanController.searchQueryTextController,
                 keyboardType: TextInputType.text,
                 decoration: InputDecoration(
                   labelText: "Input ${jikanController.mode} title",
                   icon: Icon(Icons.search),
                 ),
                 onTap: () {
-                  print(jikanController.searchQueryController.text);
+                  print(jikanController.searchQueryTextController.text);
                 },
               ),
             ),
+            //Test search
+            ElevatedButton(onPressed: () => jikanController.searchQuery(), child: Text("Search")),
             // Toggle
             Container(
               child: Row(
@@ -43,8 +45,8 @@ class Home extends StatelessWidget {
                 children: [
                   InkWell(
                     child: Container(
-                      height: 40,
-                      width: MediaQuery.of(context).size.width * 0.10,
+                      height: 30,
+                      width: 80,
                       child: Center(child: Text("Anime")),
                       decoration: BoxDecoration(
                         color: jikanController.mode.value == "Anime" ? Colors.blue : Colors.white,
@@ -57,11 +59,11 @@ class Home extends StatelessWidget {
                     ),
                     onTap: () => jikanController.mode.value = "Anime",
                   ),
-                  Container(width: 2, height: 40, color: Colors.black),
+                  Container(width: 2, height: 30, color: Colors.black),
                   InkWell(
                     child: Container(
-                      height: 40,
-                      width: MediaQuery.of(context).size.width * 0.10,
+                      height: 30,
+                      width: 80,
                       child: Center(child: Text("Manga")),
                       decoration: BoxDecoration(
                         color: jikanController.mode.value == "Manga" ? Colors.blue : Colors.white,
@@ -82,14 +84,30 @@ class Home extends StatelessWidget {
             Expanded(
               child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1),
-                child: ListView(
-                  children: (jikanController.mode.value == "Anime" ? AnimeGenres.values : MangaGenres.values)
-                      .map(
-                        (genre) => ListTile(
-                          title: Text(genre.toString().split('.').last.replaceAll('_', ' ')),
-                        ),
-                      )
-                      .toList(),
+                child: Wrap(
+                  children: jikanController.mode.value == "Anime"
+                      ? AnimeGenres.values
+                          .map(
+                            (AnimeGenres genre) => FilterChip(
+                              label: Text(genre.toString().split('.').last.replaceAll('_', ' ')),
+                              selected: jikanController.selectedAnimeGenres[genre.index],
+                              onSelected: (boolValue) {
+                                jikanController.selectedAnimeGenres[genre.index] = boolValue;
+                              },
+                            ),
+                          )
+                          .toList()
+                      : MangaGenres.values
+                          .map(
+                            (MangaGenres genre) => FilterChip(
+                              label: Text(genre.toString().split('.').last.replaceAll('_', ' ')),
+                              selected: jikanController.selectedMangaGenres[genre.index],
+                              onSelected: (boolValue) {
+                                jikanController.selectedMangaGenres[genre.index] = boolValue;
+                              },
+                            ),
+                          )
+                          .toList(),
                 ),
               ),
             ),
