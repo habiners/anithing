0a735f323480cb8d274a293f5c70be0c1415fdaa
ed <ed.paloga.20@usjr.edu.ph>
2021-07-30 20:28:25 +0800

    deleted a file

--- lib/controllers/jikan_controller.dart
+++ lib/controllers/jikan_controller.dart
@@ -7,6 +7,9 @@ import '../services/jikan_service.dart';
 class JikanController extends GetxController {
   static JikanController instance = Get.find();
 
+  JikanController({this.id});
+  RxBool isLoading = true.obs;
+  int? id;
   // Link to service
   RxList<Anime> animeList = <Anime>[].obs;
   RxList<Manga> mangaList = <Manga>[].obs;
