75debecc74c25bbf856aeb1404f8096f90f237c7
Zathelaine <bamboocosta143@gmail.com>
2021-07-31 12:35:25 +0800

    changes in anime details

--- lib/widgets/anime_details.dart
+++ lib/widgets/anime_details.dart
@@ -2,13 +2,10 @@ import 'package:anithing/controllers/jikan_controller.dart';
 import 'package:anithing/models/anime.dart';
 import 'package:anithing/services/jikan_service.dart';
 import 'package:flutter/material.dart';
-import 'package:dart_jikan/dart_jikan.dart';
 import 'package:get/get.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class AnimeDetails extends StatelessWidget {
-  // final int id;
-  // AnimeDetails({Key? key, required this.id}) : super(key: key);
   AnimeDetails({Key? key}) : super(key: key);
   final JikanController animeController = Get.find();
 
@@ -17,11 +14,12 @@ class AnimeDetails extends StatelessWidget {
     animeController.getAnime();
     return Scaffold(
       appBar: AppBar(
-        title: Text(animeController.anime!.title),
+        title: Obx(() => Text(animeController.isLoading.value
+            ? "Loading..."
+            : animeController.anime!.title)),
         leading: IconButton(
           icon: Icon(Icons.keyboard_arrow_left),
           onPressed: () {
-            // _backToList(context);
             Get.back();
           },
         ),
@@ -75,95 +73,3 @@ class AnimeDetails extends StatelessWidget {
     );
   }
 }
-
-//   void _backToList(BuildContext context) {
-//     Navigator.pop(context);
-//   }
-// }
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// FutureBuilder<Anime>(
-//         future: fetchAnime(id),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.done) {
-//             return Scaffold(
-//                 appBar: AppBar(
-//                   title: Text('${snapshot.data!.title}'),
-//                   leading: IconButton(
-//                     icon: Icon(Icons.keyboard_arrow_left),
-//                     onPressed: () {
-//                       _backToList(context);
-//                     },
-//                   ),
-//                 ),
-//                 floatingActionButton: FloatingActionButton(
-//                   onPressed: () {},
-//                   child: Icon(Icons.thumb_up),
-//                 ),
-//                 body: Column(
-//                   children: <Widget>[
-//                     Row(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: <Widget>[
-//                           Container(
-//                               child: FadeInImage.memoryNetwork(
-//                             placeholder: kTransparentImage,
-//                             image: '${snapshot.data!.imageUrl}',
-//                           ))
-//                         ]),
-//                     Row(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: <Widget>[
-//                           Container(
-//                               child:
-//                                   Icon(Icons.star, color: Colors.orangeAccent)),
-//                           Text('${snapshot.data!.score}')
-//                         ]),
-//                     Padding(
-//                         padding: EdgeInsets.all(8.0),
-//                         child: Text(
-//                           'Synopsis',
-//                           style: Theme.of(context).textTheme.headline6,
-//                         )),
-//                     Padding(
-//                       padding: EdgeInsets.symmetric(horizontal: 20.0),
-//                       child: Text('${snapshot.data!.synopsis}'),
-//                     ),
-//                     Padding(
-//                         padding: EdgeInsets.all(8.0),
-//                         child: Text(
-//                           'Genre',
-//                           style: Theme.of(context).textTheme.headline6,
-//                         )),
-//                   ],
-//                 ));
-//           } else {
-//             return Center(
-//               child: CircularProgressIndicator(),
-//             );
-//           }
-//         });
-//   }
-
-//   void _backToList(BuildContext context) {
-//     Navigator.pop(context);
-//   }
-// }
