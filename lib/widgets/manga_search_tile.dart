0b46186a6da71731e50082500f5a4de07e3f99b4
Javin <javin.tan.20@usjr.edu.ph>
2021-08-01 16:32:48 +0800

    Fixed Manga Search Tile Issue

--- lib/widgets/manga_search_tile.dart
+++ lib/widgets/manga_search_tile.dart
@@ -33,58 +33,36 @@ class MangaSearchTile extends StatelessWidget {
             Container(width: 80, height: 100, child: CachedNetworkImage(imageUrl: manga.imageUrl)),
             SizedBox(width: 4),
             Expanded(
-              child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                CustomText(text: manga.title, weight: FontWeight.bold),
-                CustomText(text: manga.synopsis, textAlign: TextAlign.left, maxLines: 3),
-                SizedBox(height: 4),
-                Row(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 4),
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    CustomText(text: manga.title, weight: FontWeight.bold),
+                    CustomText(text: manga.synopsis, textAlign: TextAlign.left, maxLines: 3),
+                    SizedBox(height: 4),
                     Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        CustomText(text: "Score: ", weight: FontWeight.bold),
-                        CustomText(text: "${manga.score}"),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        CustomText(text: "Chapters: ", weight: FontWeight.bold),
-                        CustomText(text: "${manga.chapters.toString().padLeft(4)}"),
+                        Row(
+                          children: [
+                            CustomText(text: "Score: ", weight: FontWeight.bold),
+                            CustomText(text: "${manga.score}"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            CustomText(text: "Volumes: ", weight: FontWeight.bold),
+                            CustomText(text: "${manga.volumes.toString().padLeft(4)}"),
+                          ],
+                        ),
                       ],
                     ),
                   ],
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 4),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CustomText(text: manga.title, weight: FontWeight.bold),
-                      CustomText(text: manga.synopsis, textAlign: TextAlign.left, maxLines: 3),
-                      SizedBox(height: 4),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              CustomText(text: "Score: ", weight: FontWeight.bold),
-                              CustomText(text: "${manga.score}"),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              CustomText(text: "Volumes: ", weight: FontWeight.bold),
-                              CustomText(text: "${manga.volumes.toString().padLeft(4)}"),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ]),
-            )
+              ),
+            ),
           ],
         ),
       ),
